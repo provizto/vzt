065615b8d1225ca87c49c5b2fec71f04bb0d7539
Provizto <provizto@gmail.com>
2026-05-24 13:27:57 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+// Mengubah atribut [lang] pada tag html utama berdasarkan seleksi menu drop-down
+document.getElementById('langSwitcher').addEventListener('change', function(e) {
+    document.getElementById('rootNode').setAttribute('lang', e.target.value);
+});
